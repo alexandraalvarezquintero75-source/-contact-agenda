5e86aee96206a35ad8735ab837bbc5b76f9081c3
alexandraalvarezquintero75-source <alexandraalvarezquintero75@gmail.com>
2025-10-28 17:15:22 -0500

    feat: update authentication logic to use JSON format for login request and improve error handling

--- Frontend/src/services/auth.js
+++ Frontend/src/services/auth.js
@@ -1,5 +1,3 @@
-
-
 import { useRouter } from 'vue-router'
 import api from '@/services/api'
 import Swal from 'sweetalert2'
@@ -9,33 +7,38 @@ export function auth() {
 
   const login = async (values) => {
     try {
+      console.log('Valores enviados al login:', values.user, values.password)
 
-      const formData = new URLSearchParams()
-      formData.append('username', values.user)  
-      formData.append('password', values.password)
 
-      const response = await api.post('/login', formData, {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      const response = await api.post('/auth/login', {
+        email: values.user,      // asegúrate que sea el mismo nombre que el backend espera
+        password: values.password
+      }, {
+        headers: { 'Content-Type': 'application/json' }
       })
 
       console.log('Login exitoso:', response.data)
+
+
       localStorage.setItem('access_token', response.data.access_token)
 
       await Swal.fire({
         icon: 'success',
         title: '¡Éxito!',
-        text: response.data.message,
+        text: response.data.message || 'Inicio de sesión correcto',
         confirmButtonText: 'Aceptar'
       })
 
       router.push('/dashboard')
 
     } catch (error) {
+      console.error('Error al iniciar sesión:', error)
+
       if (error.response) {
         Swal.fire({
           icon: 'error',
           title: 'Error',
-          text: error.response.data.detail,
+          text: error.response.data.detail || 'Credenciales incorrectas o error en el servidor',
           confirmButtonText: 'Aceptar'
         })
       } else {
