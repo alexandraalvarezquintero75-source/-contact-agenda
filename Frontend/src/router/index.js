7736b49e9b1c492e5282b968c3d9e421fc1f8a70
alexandraalvarezquintero75-source <alexandraalvarezquintero75@gmail.com>
2025-10-28 17:41:25 -0500

    feat: add registration view with form validation and navigation to login

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/view/auth/LoginView.vue'
 import DashboardView from '@/view/dashboard/DashboardView.vue'
+import RegisterView from '@/view/auth/RegisterView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'login',
     component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView
   },
   {
     path: '/dashboard',
---
+++ Frontend/src/view/auth/RegisterView.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container mt-5" style="max-width: 400px;">
+    <h2 class="text-center mb-3">Registro</h2>
+
+
+    <form @submit.prevent="onSubmit">
+      <!-- Email -->
+      <div class="mb-3">
+        <label for="email" class="form-label">Correo electrónico</label>
+        <input
+          id="email"
+          v-model="values.email"
+          type="email"
+          class="form-control"
+          :class="{ 'is-invalid': errors.email }"
+          placeholder="ejemplo@correo.com"
+        />
+        <div v-if="errors.email" class="invalid-feedback">{{ errors.email }}</div>
+      </div>
+
+      <!-- Contraseña -->
+      <div class="mb-3">
+        <label for="password" class="form-label">Contraseña</label>
+        <input
+          id="password"
+          v-model="values.password"
+          type="password"
+          class="form-control"
+          :class="{ 'is-invalid': errors.password }"
+          placeholder="********"
+        />
+        <div v-if="errors.password" class="invalid-feedback">{{ errors.password }}</div>
+      </div>
+
+      <button type="submit" class="btn btn-success w-100">
+        Registrar
+      </button>
+
+      <div class="text-center mt-3">
+        <span
+          class="text-primary"
+          style="cursor: pointer; text-decoration: underline;"
+          @click="goToLogin"
+        >
+          Volver al inicio de sesión
+        </span>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+
+const router = useRouter()
+
+
+const values = ref({
+  email: '',
+  password: ''
+})
+
+// Validaciones simples
+const errors = ref({})
+
+
+
+const goToLogin = () => {
+  router.push('/')
+}
+</script>
