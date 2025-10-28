89a0e2a466ceab51f29135c8771f8ee8253b7f4d
KennyLond <kennyadso15@gmail.com>
2025-10-28 11:30:18 -0500

    Add CORS middleware to enable cross-origin requests

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,13 +2,23 @@ from fastapi import FastAPI
 from app.routes import auth_routes, contacts_routes
 from app.db.dataBase import engine
 from app.models import user_model
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="ContactDesk API",
     description="API to manage contacts, users, and authentication",
     version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 app.include_router(auth_routes.router)
 app.include_router(contacts_routes.router)
