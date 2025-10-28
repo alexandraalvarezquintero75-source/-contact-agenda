2f225a5e670a410d37fa8f0c44c42b9ba638e989
alexandraalvarezquintero75-source <alexandraalvarezquintero75@gmail.com>
2025-10-28 12:20:28 -0500

    feat: implement login functionality with API integration and error handling

--- Frontend/src/services/auth.js
+++ Frontend/src/services/auth.js
@@ -1,15 +1,51 @@
+
+
 import { useRouter } from 'vue-router'
+import api from '@/services/api'
+import Swal from 'sweetalert2'
 
 export function auth() {
   const router = useRouter()
 
   const login = async (values) => {
-    const { user, password } = values
+    try {
 
-    if (user === 'admin' && password === '1234') {
+      const formData = new URLSearchParams()
+      formData.append('username', values.user)  
+      formData.append('password', values.password)
+
+      const response = await api.post('/login', formData, {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      })
+
+      console.log('Login exitoso:', response.data)
+      localStorage.setItem('access_token', response.data.access_token)
+
+      await Swal.fire({
+        icon: 'success',
+        title: '¡Éxito!',
+        text: response.data.message,
+        confirmButtonText: 'Aceptar'
+      })
+
       router.push('/dashboard')
-    } else {
-      alert('Usuario o contraseña incorrectos')
+
+    } catch (error) {
+      if (error.response) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: error.response.data.detail,
+          confirmButtonText: 'Aceptar'
+        })
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'No se pudo conectar con el servidor',
+          confirmButtonText: 'Aceptar'
+        })
+      }
     }
   }
 
