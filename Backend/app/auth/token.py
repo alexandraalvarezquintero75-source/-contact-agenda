70b544dd5af4055843e21b39fa7208a6433c60ca
KennyLond <kennyadso15@gmail.com>
2025-10-30 15:34:44 -0500

    Refactor token management: load secret key and algorithm from environment variables

--- Backend/app/auth/token.py
+++ Backend/app/auth/token.py
@@ -1,14 +1,19 @@
+import os
 from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from dotenv import load_dotenv
 from app.db.dataBase import get_db
 from app.models.user_model import User
 
-SECRET_KEY = "mi_clave_super_secreta"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
