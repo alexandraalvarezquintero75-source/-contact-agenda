d9c630956080dddc1054a3ac421e635a055c6902
alexandraalvarezquintero75-source <alexandraalvarezquintero75@gmail.com>
2025-10-31 14:37:40 -0500

    feat: add index.html and main.js files; set up initial Vue application structure with BootstrapVue3

--- Frontend/src/view/Formulario/IndexForm.vue
+++ Frontend/src/view/Formulario/IndexForm.vue
@@ -10,9 +10,8 @@
 <script setup>
 // import DataTable from '@/components/design/molecules/DataTable.vue';
 import AccionButtons from '@/components/design/molecules/AccionButtons.vue';
-
-
 import { useRouter } from 'vue-router';
+
 const router = useRouter();
 const goForm = () => {
   router.push({ name: 'form-forms' });
