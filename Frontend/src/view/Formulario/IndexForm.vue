91b25be0253c3c543ba2e8f73123fcba5b88b0b4
alexandraalvarezquintero75-source <alexandraalvarezquintero75@gmail.com>
2025-11-04 12:20:09 -0500

    feat: endpoind contact

--- Frontend/src/view/Formulario/IndexForm.vue
+++ Frontend/src/view/Formulario/IndexForm.vue
@@ -1,20 +1,61 @@
 <template>
+  <div class="position-relative">
+    <div class="position-absolute top-2 end-0 p-9">
+      <AccionButtons @save="goForm" />
+    </div>
 
-  <!-- <DataTable /> -->
-
- <div class="d-flex justify-content-end gap-2 p-3">
-    <AccionButtons @save="goForm" />
+   
+    <div class="container mt-5 pt-5">
+      <DataTable
+        :columns="columns"
+        :data="contacts"
+        @update:selected="selectedRows = $event"
+      />
+    </div>
   </div>
-
 </template>
-<script setup>
-// import DataTable from '@/components/design/molecules/DataTable.vue';
-import AccionButtons from '@/components/design/molecules/AccionButtons.vue';
-import { useRouter } from 'vue-router';
 
-const router = useRouter();
+<script setup>
+import DataTable from '@/components/design/molecules/DataTable.vue'
+import AccionButtons from '@/components/design/molecules/AccionButtons.vue'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { onMounted } from 'vue'
+import { getContacts } from '../../services/contact'
+
+const router = useRouter()
+
+const contacts = ref([])
+const selectedRows = ref([])
+
+const columns = [
+  { key: 'name', label: 'Name' },
+  { key: 'phone', label: 'Phone' }
+]
+
 const goForm = () => {
-  router.push({ name: 'form-forms' });
-};
+  router.push({ name: 'form-forms' })
+}
+onMounted(async () => {
+  try {
+    const response = await getContacts()
+console.log('Respuesta completa:', response)
+
+//  línea comentada por ahora para no causar el error
+// contacts.value = response.data.map(contact => ({
+//   name: contact.name,
+//   phone: contact.phone
+// }))
+    console.log('Contactos cargados:', contacts.value)
+  } catch (error) {
+    console.error('Error al cargar contactos:', error)
+  }
+})
+
 </script>
 
+<style scoped>
+.container {
+  margin-top: 80px; 
+}
+</style>
